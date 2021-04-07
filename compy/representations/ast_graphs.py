49ea8c7f1e073bda966af75b7b22666cb0770a84
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2021-04-07 12:19:50 +0200

    Factor out common code of AST visitors

--- compy/representations/ast_graphs.py
+++ compy/representations/ast_graphs.py
@@ -21,6 +21,55 @@ def filter_type(type):
         return "type"
 
 
+def add_ast_edges(g: nx.MultiDiGraph, node):
+    """Add edges with attr `ast` that represent the AST parent-child relationship"""
+
+    if isinstance(node, clang.graph.FunctionInfo):
+        g.add_node(node, attr="function")
+        for arg in node.args:
+            g.add_node(arg, attr=("argument", filter_type(arg.type)))
+            g.add_edge(node, arg, attr="ast")
+
+        g.add_node(node.entryStmt, attr=(node.entryStmt.name))
+        g.add_edge(node, node.entryStmt, attr="ast")
+
+    if isinstance(node, clang.graph.StmtInfo):
+        for ast_rel in node.ast_relations:
+            g.add_node(ast_rel, attr=(ast_rel.name))
+            g.add_edge(node, ast_rel, attr="ast")
+
+
+def add_ref_edges(g: nx.MultiDiGraph, node):
+    """Add edges with attr `data` for data references of the given node"""
+
+    if isinstance(node, clang.graph.StmtInfo):
+        for ref_rel in node.ref_relations:
+            g.add_node(ref_rel, attr=(filter_type(ref_rel.type)))
+            g.add_edge(node, ref_rel, attr="data")
+
+
+def add_cfg_edges(g: nx.MultiDiGraph, node):
+    """Add edges with attr `cfg` or `in` for control flow for the given node"""
+
+    if isinstance(node, clang.graph.FunctionInfo):
+        for cfg_b in node.cfgBlocks:
+            g.add_node(cfg_b, attr="cfg")
+            for succ in cfg_b.successors:
+                g.add_edge(cfg_b, succ, attr="cfg")
+                g.add_node(succ, attr="cfg")
+            for stmt in cfg_b.statements:
+                g.add_edge(stmt, cfg_b, attr="in")
+                g.add_node(stmt, attr=(stmt.name))
+
+
+def add_token_ast_edges(g: nx.MultiDiGraph, node):
+    """Add edges with attr `token` connecting tokens to the closest AST node covering them"""
+    if hasattr(node, 'tokens'):
+        for token in node.tokens:
+            g.add_node(token, attr=token.name)
+            g.add_edge(token, node, attr="token")
+
+
 class ASTVisitor(Visitor):
     def __init__(self):
         Visitor.__init__(self)
@@ -28,19 +77,7 @@ class ASTVisitor(Visitor):
         self.G = nx.MultiDiGraph()
 
     def visit(self, v):
-        if isinstance(v, clang.graph.FunctionInfo):
-            self.G.add_node(v, attr="function")
-            for arg in v.args:
-                self.G.add_node(arg, attr=("argument", filter_type(arg.type)))
-                self.G.add_edge(v, arg, attr="ast")
-
-            self.G.add_node(v.entryStmt, attr=(v.entryStmt.name))
-            self.G.add_edge(v, v.entryStmt, attr="ast")
-
-        if isinstance(v, clang.graph.StmtInfo):
-            for ast_rel in v.ast_relations:
-                self.G.add_node(ast_rel, attr=(ast_rel.name))
-                self.G.add_edge(v, ast_rel, attr="ast")
+        add_ast_edges(self.G, v)
 
 
 class ASTDataVisitor(Visitor):
@@ -50,22 +87,8 @@ class ASTDataVisitor(Visitor):
         self.G = nx.MultiDiGraph()
 
     def visit(self, v):
-        if isinstance(v, clang.graph.FunctionInfo):
-            self.G.add_node(v, attr="function")
-            for arg in v.args:
-                self.G.add_node(arg, attr=("argument", filter_type(arg.type)))
-                self.G.add_edge(v, arg, attr="ast")
-
-            self.G.add_node(v.entryStmt, attr=(v.entryStmt.name))
-            self.G.add_edge(v, v.entryStmt, attr="ast")
-
-        if isinstance(v, clang.graph.StmtInfo):
-            for ast_rel in v.ast_relations:
-                self.G.add_node(ast_rel, attr=(ast_rel.name))
-                self.G.add_edge(v, ast_rel, attr="ast")
-            for ref_rel in v.ref_relations:
-                self.G.add_node(ref_rel, attr=(filter_type(ref_rel.type)))
-                self.G.add_edge(v, ref_rel, attr="data")
+        add_ast_edges(self.G, v)
+        add_ref_edges(self.G, v)
 
 
 class ASTDataCFGVisitor(Visitor):
@@ -75,31 +98,9 @@ class ASTDataCFGVisitor(Visitor):
         self.G = nx.MultiDiGraph()
 
     def visit(self, v):
-        if isinstance(v, clang.graph.FunctionInfo):
-            self.G.add_node(v, attr="function")
-            for arg in v.args:
-                self.G.add_node(arg, attr=("argument", filter_type(arg.type)))
-                self.G.add_edge(v, arg, attr="ast")
-
-            self.G.add_node(v.entryStmt, attr=(v.entryStmt.name))
-            self.G.add_edge(v, v.entryStmt, attr="ast")
-
-            for cfg_b in v.cfgBlocks:
-                self.G.add_node(cfg_b, attr="cfg")
-                for succ in cfg_b.successors:
-                    self.G.add_edge(cfg_b, succ, attr="cfg")
-                    self.G.add_node(succ, attr="cfg")
-                for stmt in cfg_b.statements:
-                    self.G.add_edge(stmt, cfg_b, attr="in")
-                    self.G.add_node(stmt, attr=(stmt.name))
-
-        if isinstance(v, clang.graph.StmtInfo):
-            for ast_rel in v.ast_relations:
-                self.G.add_node(ast_rel, attr=(ast_rel.name))
-                self.G.add_edge(v, ast_rel, attr="ast")
-            for ref_rel in v.ref_relations:
-                self.G.add_node(ref_rel, attr=(filter_type(ref_rel.type)))
-                self.G.add_edge(v, ref_rel, attr="data")
+        add_ast_edges(self.G, v)
+        add_ref_edges(self.G, v)
+        add_cfg_edges(self.G, v)
 
 
 class ASTGraphBuilder(common.RepresentationBuilder):
